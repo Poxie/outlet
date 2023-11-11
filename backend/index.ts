5397eb3b9fa66ed0bd451364a31253685fce5976
Poxie <poxen12@hotmail.com>
2023-11-11 20:18:59 +0100

    Created simple mysql connection

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,15 @@
+import { createConnection } from './database';
 import express from 'express';
 import 'dotenv/config'
 
 const app = express();
 
-app.get(`/`, (req, res) => {
-    res.send('Hello');
+app.get('/veckans-deal', async (req, res) => {
+    const db = await createConnection();
+    
+    const [data] = await db.execute('SELECT * FROM veckans_deal');
+
+    res.send(data);
 })
 
 app.listen(process.env.PORT, () => console.log(`Backend running on ${process.env.PORT}`))
