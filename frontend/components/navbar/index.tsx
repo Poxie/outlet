c71a1cbcbe2be27fba2afe4a042794bc940b50bd
Poxie <poxen12@hotmail.com>
2023-11-13 05:41:30 +0100

    Added navbar

---
+++ frontend/components/navbar/index.tsx
@@ -0,0 +1,41 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+
+const links = [
+    { path: '/', text: 'Startsida' },
+    { path: 'Våra varuhus', text: 'Våra varuhus' },
+]
+export default function Navbar() {
+    const currentPath = usePathname();
+
+    return(
+        <div className="w-main max-w-main mx-auto flex items-center justify-between bg-secondary">
+            <Link href="/">
+                <Image 
+                    className="w-48 my-5"
+                    alt="Outlet logo"
+                    src="/logo.png"
+                    width={250}
+                    height={50} 
+                />
+            </Link>
+            <ul className="flex gap-4">
+                {links.map(link => (
+                    <li key={link.path}>
+                        <Link
+                            href={link.path}
+                            className={twMerge(
+                                "text-light text-sm"
+                            )}
+                        >
+                            {link.text}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
