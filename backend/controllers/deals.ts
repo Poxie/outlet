4dd81e553dee95c2a1183a1323a8999becdbfaaf
Poxie <poxen12@hotmail.com>
2023-11-13 05:41:12 +0100

    Changed types location

--- backend/controllers/deals.ts
+++ backend/controllers/deals.ts
@@ -1,11 +1,11 @@
+import { WeeklyDeal } from "../../types";
 import { createConnection } from "../database"
-import { WeeklyDeal } from "../types";
 
 export const getWeeklyDeal = async (dealId: string) => {
     const db = await createConnection();
 
     const [data] = await db.execute('SELECT * FROM veckans_deal WHERE id = ?', [dealId]);
     if(!Array.isArray(data)) return;
-    
+
     return data[0] as WeeklyDeal | undefined;
 }
