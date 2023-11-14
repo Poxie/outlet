b23f03dba522748506bb6c5f8cc875c4e90c3aa6
Poxie <poxen12@hotmail.com>
2023-11-14 19:42:27 +0100

    Updated events divider styles

--- frontend/components/home/events/index.tsx
+++ frontend/components/home/events/index.tsx
@@ -2,7 +2,7 @@ import { Event } from "../../../../types";
 import { EventCard } from "./EventCard";
 
 const getEvents = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/events`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/events?with_images=true`);
     const data = await res.json();
     return data as Event[];
 }
@@ -12,8 +12,8 @@ export default async function Events() {
     return(
         <section className="py-6 bg-secondary">
             <div className="w-main w-max-main mx-auto">
-                <div className="mb-3 relative flex justify-center after:absolute after:z-0 after:left-0 after:top-2/4 after:-translate-y-2/4 after:w-full after:h-[1px] after:bg-light">
-                    <span className="px-4 relative z-[1] uppercase text-xs font-bold text-light bg-secondary">
+                <div className="mb-2 relative flex after:absolute after:z-0 after:left-0 after:top-2/4 after:-translate-y-2/4 after:w-full after:h-[1px] after:bg-light">
+                    <span className="px-2 ml-4 relative z-[1] uppercase text-xs font-bold text-light bg-secondary">
                         Våra rekommendationer
                     </span>
                 </div>
