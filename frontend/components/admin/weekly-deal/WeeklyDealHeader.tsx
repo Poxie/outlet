38d4e32cac3f5f0f47cb269ce7ed8a1c082aa4f9
Poxie <poxen12@hotmail.com>
2023-11-16 23:56:41 +0100

    Increased header z-index

--- frontend/components/admin/weekly-deal/WeeklyDealHeader.tsx
+++ frontend/components/admin/weekly-deal/WeeklyDealHeader.tsx
@@ -2,7 +2,7 @@ export default function WeeklyDealHeader({ text }: {
     text: string;
 }) {
     return(
-        <div className="py-2 -mt-2 sticky top-0 bg-light after:bg-light-secondary after:h-[1px] after:w-full after:absolute after:left-0 after:top-2/4 after:-translate-y-2/4">
+        <div className="py-2 -mt-2 sticky z-[2] top-0 bg-light after:bg-light-secondary after:h-[1px] after:w-full after:absolute after:left-0 after:top-2/4 after:-translate-y-2/4">
             <span className="block relative z-[1] bg-light pr-4 text-secondary text-sm">
                 {text}
             </span>
