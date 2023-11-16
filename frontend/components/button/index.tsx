a1c58993b37380e3eb3fb72f5857abecd3beaa68
Poxie <poxen12@hotmail.com>
2023-11-16 22:19:33 +0100

    Added horizontal padding to button

--- frontend/components/button/index.tsx
+++ frontend/components/button/index.tsx
@@ -11,7 +11,7 @@ export default function Button({
     className?: string;
 }) {
     className = twMerge(
-        "flex items-center justify-center gap-1.5 py-3 text-xs font-bold text-light bg-secondary rounded-md transition-colors hover:bg-opacity-80",
+        "flex items-center justify-center gap-1.5 py-3 px-4 text-xs font-bold text-light bg-secondary rounded-md transition-colors hover:bg-opacity-80",
         className,
     );
 
