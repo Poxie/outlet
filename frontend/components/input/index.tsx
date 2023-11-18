4861fbdc39d7c75eb3e83ef72c234a2607fa851c
Poxie <poxen12@hotmail.com>
2023-11-18 04:03:43 +0100

    Created input component

---
+++ frontend/components/input/index.tsx
@@ -0,0 +1,32 @@
+import { forwardRef } from 'react';
+
+const Input = forwardRef<HTMLInputElement, {
+    onChange: (value: string) => void;
+    value: string | number;
+    placeholder?: string;
+    autoFocus?: boolean;
+    type?: 'text' | 'password' | 'number';
+    className?: string
+    containerClassName?: string;
+    textArea?: boolean;
+}>(({ onChange, value, placeholder, autoFocus, textArea=false, type='text', className='' }, ref) => {
+    const props = {
+        placeholder,
+        autoFocus,
+        value,
+        type,
+        ref: ref as any,
+        autoComplete: 'off',
+        className: "p-3 bg-tertiary border-[1px] bg-light-secondary border-light-tertiary outline-none rounded-md w-full text-sm " + className,
+        onChange: (e: React.ChangeEvent<any>) => onChange(e.target.value),
+        'aria-placeholder': placeholder,
+    }
+    return(
+        !textArea ? (
+            <input {...props} />
+        ) : (
+            <textarea {...props}></textarea>
+        )
+    )
+});
+export default Input;
