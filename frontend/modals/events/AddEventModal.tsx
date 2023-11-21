23af01384921fdaf74a2563aa3d788557e5e456c
Poxie <poxen12@hotmail.com>
2023-11-21 22:29:29 +0100

    Made add event modal prop required

--- frontend/modals/events/AddEventModal.tsx
+++ frontend/modals/events/AddEventModal.tsx
@@ -7,7 +7,7 @@ import { Event } from "../../../types";
 import EventModalContent from "./EventModalContent";
 
 export default function AddEventModal({ onEventAdd }: {
-    onEventAdd?: (event: Event) => void;
+    onEventAdd: (event: Event) => void;
 }) {
     const { post } = useAuth();
     const { close: closeModal } = useModal();
@@ -27,7 +27,7 @@ export default function AddEventModal({ onEventAdd }: {
         });
 
         closeModal();
-        if(onEventAdd) onEventAdd(createdEvent);
+        onEventAdd(createdEvent);
     }
 
     return(
