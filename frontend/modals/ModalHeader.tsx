d19b948abf571e72b8b52e6eea672bfaaf1447b9
Poxie <poxen12@hotmail.com>
2023-11-22 00:11:20 +0100

    Added confirm modal

--- frontend/modals/ModalHeader.tsx
+++ frontend/modals/ModalHeader.tsx
@@ -1,9 +1,18 @@
-export default function ModalHeader({ children }: {
+export default function ModalHeader({ children, subHeader, className }: {
     children: React.ReactNode;
+    subHeader?: React.ReactNode;
+    className?: string;
 }) {
     return(
         <div className="py-3.5 px-4 border-b-[1px] border-b-light-secondary">
-            {children}
+            <span className={className}>
+                {children}
+            </span>
+            {subHeader && (
+                <span className="mt-1 inline-block text-sm text-secondary">
+                    {subHeader}
+                </span>
+            )}
         </div>
     )
 }
---
+++ frontend/modals/confirm/index.tsx
@@ -0,0 +1,51 @@
+import Button from "@/components/button";
+import Modal from "../Modal";
+import ModalHeader from "../ModalHeader";
+import { useState } from "react";
+import { useModal } from "@/contexts/modal";
+
+export default function ConfirmModal<T>({ header, subHeader, cancelText='Cancel', confirmText='Confirm', confirmLoadingText='Confirming...', confirmFunction, onConfirm }: {
+    header: string;
+    subHeader?: string;
+    cancelText?: string;
+    confirmText?: string;
+    confirmLoadingText?: string;
+    confirmFunction: () => Promise<T>;
+    onConfirm: (data: T) => void;
+}) {
+    const { close } = useModal();
+
+    const [loading, setLoading] = useState(false);
+
+    const handleConfirm = async () => {
+        setLoading(true);
+        const data = await confirmFunction();
+        onConfirm(data);
+        close();
+    }
+
+    return(
+        <Modal>
+            <ModalHeader 
+                subHeader={subHeader}
+                className="text-lg font-semibold"
+            >
+                {header}
+            </ModalHeader>
+            <div className="p-4 flex justify-end gap-2 bg-light-secondary/40">
+                <Button 
+                    className="bg-transparent text-secondary"
+                    disabled={loading}
+                >
+                    {cancelText}
+                </Button>
+                <Button
+                    onClick={handleConfirm}
+                    disabled={loading}
+                >
+                    {!loading ? confirmText : confirmLoadingText}
+                </Button>
+            </div>
+        </Modal>
+    )
+}
