72d9f2a03ee1bb79611e2e1921cd9cffc1e61f77
Poxie <poxen12@hotmail.com>
2023-11-22 00:43:53 +0100

    Made table sections collapsable

--- frontend/components/admin/events/EventTableSection.tsx
+++ frontend/components/admin/events/EventTableSection.tsx
@@ -5,6 +5,9 @@ import Image from "next/image";
 import { getEventImage, getReadableDateFromTimestamp } from "@/utils";
 import EventTableOptions from "./EventTableOptions";
 import Button from "@/components/button";
+import { ArrowIcon } from "@/assets/icons/ArrowIcon";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export default function EventTableSection({ events, header }: {
     events: Event[];
@@ -12,23 +15,42 @@ export default function EventTableSection({ events, header }: {
 }) {
     const { search, removeEvent, editEvent } = useEvents();
 
+    const [expanded, setExpanded] = useState(true);
+
+    const toggleExpanded = () => setExpanded(prev => !prev);
+
     return(
         <>
-        <tr className="sticky -top-[1px] text-left rounded-lg border-b-[1px] border-b-light-secondary bg-light">
+        <tr 
+            className="sticky -top-[1px] text-left rounded-lg border-b-[1px] border-b-light-secondary bg-light hover:bg-light-secondary/40 transition-colors"
+            onClick={toggleExpanded}
+        >
             <td className="px-4 py-[--spacing] font-bold">
                 {!search ? header : `Found ${events.length} results based on filters`}
             </td>
-            <td className="px-4 py-[--spacing] font-bold">
+            <td className="w-[50%] px-4 py-[--spacing] font-bold">
                 Description
             </td>
             <td className="px-4 py-[--spacing] font-bold">
                 Date
             </td>
-            <td></td>
+            <td className="px-4 py-[--spacing]">
+                <div className="flex justify-end">
+                    <button 
+                        className="p-1"
+                        aria-label={expanded ? 'Hide events' : 'Show events'}
+                    >
+                        <ArrowIcon className={twMerge(
+                            "w-4 rotate-90 transition-transform duration-200",
+                            expanded && "rotate-180"
+                        )} />
+                    </button>
+                </div>
+            </td>
         </tr>
-        {events.map(event => (
+        {expanded && events.map(event => (
             <tr 
-                className="border-b-[1px] border-b-light-secondary/80 last:border-b-0"
+                className="border-b-[1px] border-b-light-secondary/80 last:border-b-0 h-0"
                 key={event.id}
             >
                 <td className="px-4 py-4">
@@ -43,7 +65,7 @@ export default function EventTableSection({ events, header }: {
                         {event.title}
                     </div>
                 </td>
-                <td className="px-4 py-4 text-secondary w-[50%]">
+                <td className="px-4 py-4 text-secondary">
                     <span className="line-clamp-2">
                         {event.description}
                     </span>
