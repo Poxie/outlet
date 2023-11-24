b3619d24f8e827dd4509bc39a2fc424d85a15c5f
Poxie <poxen12@hotmail.com>
2023-11-24 03:28:13 +0100

    Preventing fetch if data is already fetched

--- frontend/components/admin/events/index.tsx
+++ frontend/components/admin/events/index.tsx
@@ -4,16 +4,18 @@ import EventPanel from "./EventPanel";
 import { useEffect } from "react";
 import { setEvents } from "@/store/slices/events";
 import { useAuth } from "@/contexts/auth";
-import { useAppDispatch } from "@/store";
+import { useAppDispatch, useAppSelector } from "@/store";
 
 export default function Events() {
     const { get } = useAuth();
 
     const dispatch = useAppDispatch();
+    const eventLength = useAppSelector(state => state.events.events.length);
 
     useEffect(() => {
+        if(eventLength) return;
         get(`/events/all`).then(events => dispatch(setEvents(events)));
-    }, []);
+    }, [eventLength]);
 
     return(
         <main className="relative my-12 flex flex-col max-h-[750px] min-h-[500px] w-main max-w-main mx-auto rounded-lg overflow-auto bg-light">
