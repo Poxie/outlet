d418641de61fd9ffbe0abb715e799c16b8fe6f26
Poxie <poxen12@hotmail.com>
2023-11-27 05:50:52 +0100

    Allowed admin header overflow

--- frontend/components/admin/AdminHeader.tsx
+++ frontend/components/admin/AdminHeader.tsx
@@ -6,7 +6,7 @@ export default function AdminHeader({ text, backPath }: {
     backPath: string;
 }) {
     return(
-        <div className="sticky top-0 z-10 flex bg-light border-b-[1px] border-b-light-secondary rounded-t-lg overflow-hidden">
+        <div className="sticky top-0 z-10 flex bg-light border-b-[1px] border-b-light-secondary rounded-t-lg">
             <Link 
                 href={backPath}
                 className="p-3.5 flex items-center gap-1 text-sm hover:bg-light-secondary transition-colors"
