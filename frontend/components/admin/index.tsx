bc85ea4477ecce7faabf9515252c884ab52588bd
Poxie <poxen12@hotmail.com>
2023-11-27 03:49:12 +0100

    Redesigned admin dashboard

--- frontend/components/admin/index.tsx
+++ frontend/components/admin/index.tsx
@@ -1,10 +1,21 @@
 import { ArrowIcon } from "@/assets/icons/ArrowIcon"
+import { BannersIcon } from "@/assets/icons/BannersIcon"
+import { EventsIcon } from "@/assets/icons/EventsIcon"
+import { InspirationIcon } from "@/assets/icons/InspirationIcon"
+import { PeopleIcon } from "@/assets/icons/PeopleIcon"
+import { StoresIcon } from "@/assets/icons/StoresIcon"
+import { WeeklyDealIcon } from "@/assets/icons/WeeklyDealIcon"
 import Link from "next/link"
 
+const dashboardLinks = [
+    { text: 'Veckans deal', path: '/veckans-deal', icon: <WeeklyDealIcon className="w-6" /> },
+    { text: 'Events', path: '/events', icon: <EventsIcon className="w-6" /> },
+    { text: 'Inspiration', path: '/inspiration', icon: <InspirationIcon className="w-6" /> },
+    { text: 'Banners', path: '/banners', icon: <BannersIcon className="w-6" /> },
+]
 const adminLinks = [
-    { text: 'Veckans deal', path: '/veckans-deal' },
-    { text: 'Events', path: '/events' },
-    { text: 'Banners', path: '/banners' },
+    { text: 'Stores', path: '/stores', icon: <StoresIcon className="w-5 -mt-0.5" /> },
+    { text: 'People', path: '/people', icon: <PeopleIcon className="w-5 -mt-0.5" /> },
 ]
 
 export default function Admin() {
@@ -13,19 +24,38 @@ export default function Admin() {
             <h1 className="text-light font-bold text-4xl text-center mb-4">
                 Admin Panel
             </h1>
-            <ul className="grid gap-2 bg-light p-6 rounded-lg">
-                {adminLinks.map(link => (
-                    <li key={link.path}>
-                        <Link
-                            className="flex items-center justify-between p-5 bg-light-secondary hover:shadow-md transition-shadow rounded-md font-semibold text-lg"
-                            href={`/admin/${link.path}`}
-                        >
-                            {link.text}
-                            <ArrowIcon className="w-5 rotate-90" />
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+            <div className="bg-light p-4 rounded-lg">
+                <ul className="grid grid-cols-2 gap-2">
+                    {dashboardLinks.map(link => (
+                        <li key={link.path}>
+                            <Link
+                                className="flex items-center justify-between p-7 border-[1px] border-light-tertiary bg-light-secondary hover:shadow-md transition-shadow rounded-md font-medium text-primary text-lg"
+                                href={`/admin/${link.path}`}
+                            >
+                                {link.text}
+                                {link.icon}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+                <hr className="my-4 h-[1px] bg-light-secondary" />
+                <ul className="grid gap-1.5">
+                    {adminLinks.map(link => (
+                        <li key={link.path}>
+                            <Link
+                                className="p-3 flex items-center justify-between border-[1px] border-light-tertiary bg-light-secondary hover:shadow-md transition-shadow rounded-md font-medium text-primary"
+                                href={`/admin/${link.path}`}
+                            >
+                                <div className="flex items-center gap-2">
+                                    {link.icon}
+                                    {link.text}
+                                </div>
+                                <ArrowIcon className="w-4 rotate-90" />
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </main>
     )
 }
