0c40ecb3a44862c5654061a85cf00a7e63a0c721
Poxie <poxen12@hotmail.com>
2023-11-27 10:40:42 +0100

    Complying with new possible undefined state

--- frontend/store/slices/deals.ts
+++ frontend/store/slices/deals.ts
@@ -18,15 +18,21 @@ export const dealsSlice = createSlice({
             state.loading = false;
         },
         addDeal: (state, action) => {
-            state.deals[action.payload.date].unshift(action.payload);
+            const deals = state.deals[action.payload.date];
+            if(!deals) {
+                state.deals[action.payload.date] = [action.payload];
+                return;
+            }
+
+            deals.unshift(action.payload);
         },
         removeDeal: (state, action) => {
             const date = action.payload.date;
-            state.deals[date] = state.deals[date].filter(deal => deal.id !== action.payload.dealId);
+            state.deals[date] = (state.deals[date] || []).filter(deal => deal.id !== action.payload.dealId);
         },
         editDeal: (state, action) => {
             const date = action.payload.date;
-            state.deals[date] = state.deals[date].map(deal => {
+            state.deals[date] = (state.deals[date] || []).map(deal => {
                 if(deal.id !== action.payload.dealId) return deal;
                 return {...deal, ...action.payload.changes};
             });
