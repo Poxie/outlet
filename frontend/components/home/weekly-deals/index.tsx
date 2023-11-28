ed9146e82794e64b6626057ce5c56cffc9738031
Poxie <poxen12@hotmail.com>
2023-11-28 20:51:02 +0100

    Moved weekly deal info into separate island

--- frontend/components/home/weekly-deals/index.tsx
+++ frontend/components/home/weekly-deals/index.tsx
@@ -3,6 +3,7 @@ import { WeeklyDeal } from '../../../../types';
 import Link from 'next/link';
 import Carousel from '@/components/carousel';
 import { getWeeklyDealImage } from '@/utils';
+import { ArrowIcon } from '@/assets/icons/ArrowIcon';
 
 const getWeeklyDeals = async () => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/weekly-deals`, { next: { revalidate: 0 } });
@@ -32,17 +33,18 @@ export default async function WeeklyDeals() {
                     }))}
                     itemsPerPage={3}
                 />
-                <div className="flex items-center justify-between text-sm pt-3">
-                    <span className="text-secondary">
-                        Gäller t.o.m. {endString}. Risk för utsäljning.
-                    </span>
-                    <Link 
-                        className="text-c-primary"
-                        href={`/veckans-deal`}
-                    >
-                        Visa alla deals
-                    </Link>
-                </div>
+            </div>
+            <div className="mt-1.5 p-2 w-main max-w-main mx-auto flex items-center justify-between text-sm bg-light rounded-lg">
+                <span className="p-2 text-secondary">
+                    Gäller t.o.m. {endString}. Risk för utsäljning.
+                </span>
+                <Link 
+                    className="p-2 flex gap-1 text-c-primary rounded hover:bg-light-secondary/60 active:bg-light-tertiary/60 transition-colors"
+                    href={`/veckans-deal`}
+                >
+                    Visa alla deals
+                    <ArrowIcon className="-mr-0.5 w-4 rotate-90" />
+                </Link>
             </div>
         </section>
     )
