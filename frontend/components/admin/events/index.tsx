fceb48de37c03b6c75143db668d0c03f9d23f2fe
Poxie <poxen12@hotmail.com>
2023-11-28 19:14:12 +0100

    Removed set heights from events page

--- frontend/components/admin/events/index.tsx
+++ frontend/components/admin/events/index.tsx
@@ -8,7 +8,7 @@ export default function Events() {
     return(
         <main className="my-12 w-main max-w-main mx-auto">
             <AdminTabs />
-            <div className="relative flex flex-col max-h-[750px] min-h-[500px] rounded-lg overflow-auto bg-light">
+            <div className="relative flex flex-col rounded-lg overflow-hidden bg-light">
                 <AdminHeader 
                     backPath={'/admin'}
                     text={'Events'}
