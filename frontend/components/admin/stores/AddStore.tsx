06670a594820e4c984d36f064d7b1b904e06fae3
Poxie <poxen12@hotmail.com>
2023-11-29 03:13:31 +0100

    Created add store layout, added store info inputs

--- frontend/components/admin/stores/AddStore.tsx
+++ frontend/components/admin/stores/AddStore.tsx
@@ -1,3 +1,5 @@
+"use client";
+import Input from "@/components/input";
 import AdminHeader from "../AdminHeader";
 import AdminTabs from "../AdminTabs";
 
@@ -5,12 +7,72 @@ export default function AddStore() {
     return(
         <main className="py-8 w-main max-w-main mx-auto">
             <AdminTabs />
-            <div className="rounded-lg overflow-hidden bg-light">
+            <div className="bg-light rounded-lg overflow-hidden">
                 <AdminHeader 
                     backPath={'/admin/stores'}
                     text={'Add store'}
                 />
-                add store
+                <div className="grid grid-cols-[1fr_1px_1fr]">
+                    <div className="flex-1">
+                        <div className="p-4 flex border-b-[1px] border-b-light-secondary">
+                            <span className="text-sm text-secondary">
+                                Store info
+                            </span>
+                        </div>
+                        <div className="p-4 grid gap-3">
+                            <div className="grid gap-1">
+                                <span className="text-sm text-secondary">
+                                    Store name
+                                    {' '}
+                                    <span className="text-c-primary">
+                                        *
+                                    </span>
+                                </span>
+                                <Input 
+                                    placeholder={'Store name'}
+                                    onChange={console.log}
+                                />
+                            </div>
+                            <div className="grid gap-1">
+                                <span className="text-sm text-secondary">
+                                    Store address
+                                    {' '}
+                                    <span className="text-c-primary">
+                                        *
+                                    </span>
+                                </span>
+                                <Input 
+                                    placeholder={'Store address'}
+                                    onChange={console.log}
+                                    minHeight={80}
+                                    textArea
+                                />
+                            </div>
+                            <div className="grid gap-1">
+                                <span className="text-sm text-secondary">
+                                    Store phone number
+                                </span>
+                                <Input 
+                                    placeholder={'Store phone number'}
+                                    onChange={console.log}
+                                />
+                            </div>
+                            <div className="grid gap-1">
+                                <span className="text-sm text-secondary">
+                                    Store email
+                                </span>
+                                <Input 
+                                    placeholder={'Store email'}
+                                    onChange={console.log}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="self-stretch bg-light-secondary" />
+                    <div className="flex-1">
+
+                    </div>
+                </div>
             </div>
         </main>
     )
