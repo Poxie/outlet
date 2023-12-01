ac568a5db8a0140f06df2c269f171e1c32cea8e5
Poxie <poxen12@hotmail.com>
2023-12-02 00:37:06 +0100

    Fixed inspiration archived & unarchive buttons

--- frontend/components/admin/inspiration/InspirationOptions.tsx
+++ frontend/components/admin/inspiration/InspirationOptions.tsx
@@ -1,15 +1,14 @@
+import Link from "next/link";
+import ConfirmModal from "@/modals/confirm";
 import { ArchiveIcon } from "@/assets/icons/ArchiveIcon";
 import { BinIcon } from "@/assets/icons/BinIcon";
 import { EditIcon } from "@/assets/icons/EditIcon";
 import { useAuth } from "@/contexts/auth";
 import { useModal } from "@/contexts/modal";
-import { useEvents } from "@/hooks/useEvents";
-import ConfirmModal from "@/modals/confirm";
-import { Event } from "../../../../types";
-import Link from "next/link";
 import { UnarchiveIcon } from "@/assets/icons/UnarchiveIcon";
 import { useAppDispatch } from "@/store";
-import { removeInspiration } from "@/store/slices/inspiration";
+import { editInspiration, removeInspiration } from "@/store/slices/inspiration";
+import { BlogPost } from "../../../../types";
 
 export default function InspirationOptions({ isArchived, postId }: {
     isArchived: boolean;
@@ -17,7 +16,6 @@ export default function InspirationOptions({ isArchived, postId }: {
 }) {
     const { _delete, patch } = useAuth();
     const { setModal } = useModal();
-    const { editEvent, archiveEvent, unarchiveEvent } = useEvents();
 
     const dispatch = useAppDispatch();
 
@@ -37,39 +35,39 @@ export default function InspirationOptions({ isArchived, postId }: {
         )
     }
     const openArchiveModal = () => {
-        const onConfirm = (event: Event) => archiveEvent(event.id);
+        const onConfirm = (event: BlogPost) => dispatch(editInspiration({ inspirationId: postId, changes: { archived: true } }));
         const confirmFunction = async () => {
-            const data = await patch<Event>(`/inspiration/${postId}`, { archived: true });
+            const data = await patch<BlogPost>(`/inspiration/${postId}`, { archived: true });
             return data;
         }
 
         setModal(
             <ConfirmModal 
-                header={'Are you sure you want to archive this event?'}
-                subHeader={`Archiving this event will leave it here in the event panel, but will be hidden from visitors to the site. Don't worry, this can be undone.`}
+                header={'Are you sure you want to archive this post?'}
+                subHeader={`Archiving this post will leave it here in the post panel, but will be hidden from visitors to the site. Don't worry, this can be undone.`}
                 confirmFunction={confirmFunction}
                 onConfirm={onConfirm}
-                confirmText={'Archive event'}
-                confirmLoadingText={'Archiving event...'}
+                confirmText={'Archive post'}
+                confirmLoadingText={'Archiving post...'}
                 closeOnCancel
             />
         )
     }
     const openUnarchiveModal = () => {
-        const onConfirm = (event: Event) => unarchiveEvent(event.id);
+        const onConfirm = (post: BlogPost) => dispatch(editInspiration({ inspirationId: postId, changes: { archived: false } }));
         const confirmFunction = async () => {
-            const data = await patch<Event>(`/inspiration/${postId}`, { archived: false });
+            const data = await patch<BlogPost>(`/inspiration/${postId}`, { archived: false });
             return data;
         }
 
         setModal(
             <ConfirmModal 
-                header={'Are you sure you want to restore this event?'}
-                subHeader={`Restoring this event will make it visible to users visiting the site again, as long as it\'s not scheduled.`}
+                header={'Are you sure you want to restore this post?'}
+                subHeader={`Restoring this post will make it visible to users visiting the site again, as long as it\'s not scheduled.`}
                 confirmFunction={confirmFunction}
                 onConfirm={onConfirm}
-                confirmText={'Restore event'}
-                confirmLoadingText={'Restoring event...'}
+                confirmText={'Restore post'}
+                confirmLoadingText={'Restoring post...'}
                 closeOnCancel
             />
         )
