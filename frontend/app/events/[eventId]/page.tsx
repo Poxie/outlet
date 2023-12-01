b79354fc8016543eeb975b35c1e508d3d8a47239
Poxie <poxen12@hotmail.com>
2023-12-01 04:23:38 +0100

    Added meta data to event pages

--- frontend/app/events/[eventId]/page.tsx
+++ frontend/app/events/[eventId]/page.tsx
@@ -1,3 +1,31 @@
 import Event from "@/components/event";
+import { Event as EventType } from "../../../../types";
+import { getEventImage } from "@/utils";
+
+const basePath = (eventId: string) => `${process.env.NEXT_PUBLIC_API_ENDPOINT}/events/${eventId}`;
+const opts = { next: { revalidate: 0 } };
+
+export async function generateMetadata({ 
+    params: { eventId },
+    searchParams: { imageId },
+}: {
+    params: { eventId: string };
+    searchParams: { imageId?: string };
+}) {
+    const res = await fetch(basePath(eventId), opts);
+    const event = await res.json() as EventType;
+
+    const title = `${event.title} - Åhlens Outlet`;
+    const description = event.description;
+    return {
+        title,
+        description,
+        openGraph: {
+            title,
+            description,
+            images: imageId ? [getEventImage(eventId, imageId, event.timestamp)] : [],
+        }
+    }
+}
 
 export default Event;
