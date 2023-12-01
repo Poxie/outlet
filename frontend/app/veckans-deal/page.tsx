568071b46a5fb5327afc77197683041d02524a12
Poxie <poxen12@hotmail.com>
2023-12-01 04:53:40 +0100

    Added metadata to weekly deals page

--- frontend/app/veckans-deal/page.tsx
+++ frontend/app/veckans-deal/page.tsx
@@ -1,3 +1,16 @@
 import WeeklyDeals from "@/components/weekly-deals";
+import { Metadata } from "next";
+
+const title = 'Veckans deal - Åhlens Outlet';
+const description = 'Här är denna veckas specialerbjudanden! Gäller till och med söndag. Erbjudanden kan skilja mellan varuhusen. Risk för utsäljning!'
+export const metadata: Metadata = {
+    title,
+    description,
+    openGraph: {
+        title,
+        description,
+        images: [`${process.env.NEXT_PUBLIC_API_ENDPOINT}/weekly-deals/image.png`],
+    }
+}
 
 export default WeeklyDeals;
