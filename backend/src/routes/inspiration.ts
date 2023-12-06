c53792b11a7e151b50b3d746b9b13778a9dcb0b5
Poxie <poxen12@hotmail.com>
2023-12-06 23:34:19 +0100

    Added images to inspiration posts

--- backend/src/routes/inspiration.ts
+++ backend/src/routes/inspiration.ts
@@ -1,16 +1,33 @@
+// @ts-ignore
+import * as imageDataURI from 'image-data-uri';
 import * as express from 'express';
+import * as fs from 'fs';
 import { myDataSource } from '../app-data-source';
 import { Inspiration } from '../entity/inspiration.entity';
 import { ALLOWED_INSPIRATION_PROPERTIES, REQUIRED_INSPIRATION_PROPERTIES } from '../utils/constants';
 import { APIBadRequestError } from '../errors/apiBadRequestError';
 import { createId } from '../utils';
 import { APINotFoundError } from '../errors/apiNotFoundError';
+import { Images } from '../entity/images.entity';
+import { APIInternalServerError } from '../errors/apiInternalServerError';
 
 const router = express.Router();
 
 router.get('/inspiration', async (req, res, next) => {
     const inspiration = await myDataSource.getRepository(Inspiration).find();
-    res.send(inspiration);
+
+    const inspirationWithImages = [];
+    for(const post of inspiration) {
+        const images = await myDataSource.getRepository(Images).findBy({
+            parentId: post.id,
+        })
+        inspirationWithImages.push({
+            ...post,
+            images: images.sort((a,b) => a.position - b.position),
+        })
+    }
+
+    res.send(inspirationWithImages);
 })
 router.post('/inspiration', async (req, res, next) => {
     for(const prop of REQUIRED_INSPIRATION_PROPERTIES) {
@@ -54,6 +71,122 @@ router.delete('/inspiration/:inspirationId', async (req, res, next) => {
     if(!inspiration) return next(new APINotFoundError('Post not found.'));
 
     await myDataSource.getRepository(Inspiration).delete(inspiration);
+
+    res.send({});
+})
+router.post('/inspiration/:inspirationId/images', async (req, res, next) => {
+    const post = await myDataSource.getRepository(Inspiration).findOneBy({ id: req.params.inspirationId });
+    if(!post) return next(new APINotFoundError('Post not found.'));
+
+    const images = req.body.images;
+    if(!images) return next(new APINotFoundError('Images property is required.'));
+
+    const currentImageCount = await myDataSource.getRepository(Images)
+        .createQueryBuilder('images')
+        .where('images.parentId = :parentId', { parentId: post.id })
+        .orderBy('images.positions', 'DESC')
+        .getCount();
+
+    const newImages: Images[] = [];
+    for(let i = 0; i < images.length; i++) {
+        const image = images[i];
+        const imageId = await createId('images');
+    
+        const imagePath = `src/imgs/blog/${post.id}/${imageId}.png`;
+        let imageResponse: string;
+        try {
+            imageResponse = await imageDataURI.outputFile(image, imagePath);
+        } catch(error) {
+            console.error(`Unable to save image ${imagePath}.`, error);
+            return next(new APIInternalServerError('Unable to save image.'));
+        }
+    
+        const createdImage = myDataSource.getRepository(Images).create({
+            id: imageId,
+            image: imageId,
+            parentId: post.id,
+            timestamp: post.timestamp,
+            position: currentImageCount + i,
+        });
+        const newImage = await myDataSource.getRepository(Images).save(createdImage);
+    
+        newImages.push(newImage);
+    }
+
+    res.send(newImages);
+})
+router.patch('/inspiration/:inspirationId/images/positions', async (req, res, next) => {
+    const post = await myDataSource.getRepository(Inspiration).findOneBy({ id: req.params.inspirationId });
+    if(!post) return next(new APINotFoundError('Post not found.'));
+
+    const positions = req.body.positions;
+    if(!positions) return next(new APIBadRequestError('Positions is required.'));
+    if(!Array.isArray(positions)) return next(new APIBadRequestError('Positions must be an array.'));
+
+    let prevPositions: number[] = [];
+    for(const item of positions.sort((a,b) => a.position - b.position)) {
+        if(item.position === undefined || item.id === undefined) {
+            return next(new APIBadRequestError('Array items must have an id and a position.'));
+        }
+        if(!prevPositions.length) {
+            if(item.position !== 0) return next(new APIBadRequestError('First position must be 0.'));
+            prevPositions.push(item.position);
+            continue;
+        }
+
+        if(item.position !== prevPositions.at(-1) + 1) return next(new APIBadRequestError('Positions must be in a consecutive order.'));
+        prevPositions.push(item.position)
+    }
+
+    for(const item of positions) {
+        await myDataSource.getRepository(Images).update({
+            id: item.id,
+        }, {
+            position: item.position,
+        })
+    }
+
+    const images = await myDataSource.getRepository(Images)
+        .createQueryBuilder('images')
+        .where('images.parentId = :parentId', { parentId: post.id })
+        .orderBy('images.position', 'ASC')
+        .getMany();
+        
+    res.send(images);
+})
+router.delete('/inspiration/:inspirationId/images', async (req, res, next) => {
+    const post = await myDataSource.getRepository(Inspiration).findOneBy({ id: req.params.inspirationId });
+    if(!post) return next(new APINotFoundError('Post not found.'));
+
+    const ids = req.body.ids;
+    if(!ids) return next(new APIBadRequestError("Ids property is required."));
+
+    for(const id of ids) {
+        const image = await myDataSource.getRepository(Images).findOneBy({ id });
+        if(!image) return next(new APINotFoundError(`Image with id of "${id}" was not found.`));
+
+        const imagePath = `src/imgs/blog/${image.parentId}/${image.id}.png`;
+        try {
+            fs.rmSync(imagePath);
+        } catch(error) {
+            console.error(`Unable to remove image: ${imagePath}.`);
+        }
+
+        await myDataSource.getRepository(Images).delete({ id: image.id });
+        
+        // Updating other images' positions
+        const imagesToUpdate = await myDataSource.getRepository(Images)
+            .createQueryBuilder('images')
+            .where('images.position > :position AND images.parentId = :parentId', { position: image.position, parentId: post.id })
+            .getMany();
+
+        for(const imageToUpdate of imagesToUpdate) {
+            await myDataSource.getRepository(Images).save({
+                ...imageToUpdate,
+                position: imageToUpdate.position - 1,
+            })
+        }
+    }
 
     res.send({});
 })
