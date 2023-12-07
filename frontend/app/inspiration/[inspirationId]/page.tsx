1febd797a039d2ba0d014137b0472f9edb519bc4
Poxie <poxen12@hotmail.com>
2023-12-07 23:02:46 +0100

    Added meta tags to inspiration post

--- frontend/app/inspiration/[inspirationId]/page.tsx
+++ frontend/app/inspiration/[inspirationId]/page.tsx
@@ -1,3 +1,32 @@
 import Inspiration from "@/components/inspiration/Inspiration";
+import { BlogPost } from "../../../../types";
+import { getBlogImage } from "@/utils";
+
+const basePath = (inspirationId: string) => `${process.env.NEXT_PUBLIC_API_ENDPOINT}/inspiration/${inspirationId}`;
+const opts = { next: { revalidate: 0 } };
+
+export async function generateMetadata({ 
+    params: { inspirationId },
+    searchParams: { photo },
+}: {
+    params: { inspirationId: string };
+    searchParams: { photo?: string };
+}) {
+    const res = await fetch(basePath(inspirationId), opts);
+    const post = await res.json() as BlogPost;
+
+    const title = `${post.title} - Åhlens Outlet`;
+    const description = post.description;
+    return {
+        title,
+        description,
+        openGraph: {
+            url: 
+            title,
+            description,
+            images: photo ? [getBlogImage(post.id, photo)] : [],
+        }
+    }
+}
 
 export default Inspiration;
