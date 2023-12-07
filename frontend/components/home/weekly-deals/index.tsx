c57483e53c36d37850673ecd85be668e69026f29
Poxie <poxen12@hotmail.com>
2023-12-07 22:26:27 +0100

    Added lighter background to deals on home page

--- frontend/components/home/weekly-deals/index.tsx
+++ frontend/components/home/weekly-deals/index.tsx
@@ -24,33 +24,35 @@ export default async function WeeklyDeals() {
     const endString = `${date.getDate()} ${month}`;
 
     return(
-        <section className="py-8 w-main max-w-main mx-auto">
-            <div className="mb-2 p-2 flex items-center justify-between text-sm bg-light rounded-lg">
-                <div className="px-2 flex items-center gap-3">
-                    <span className="text-xl font-semibold">
-                        Veckans deal
-                    </span>
-                    <span className="text-secondary mt-1">
-                        Gäller t.o.m. {endString}. Risk för utsäljning.
-                    </span>
+        <section className="bg-secondary">
+            <div className="py-8 w-main max-w-main mx-auto">
+                <div className="mb-2 p-2 flex items-center justify-between text-sm bg-light rounded-lg">
+                    <div className="px-2 flex items-center gap-3">
+                        <span className="text-xl font-semibold">
+                            Veckans deal
+                        </span>
+                        <span className="text-secondary mt-1">
+                            Gäller t.o.m. {endString}. Risk för utsäljning.
+                        </span>
+                    </div>
+                    <Link 
+                        className="p-2 flex gap-1 text-c-primary rounded hover:bg-light-secondary/60 active:bg-light-tertiary/60 transition-colors"
+                        href={`/veckans-deal`}
+                    >
+                        Visa alla deals
+                        <ArrowIcon className="-mr-0.5 w-4 rotate-90" />
+                    </Link>
                 </div>
-                <Link 
-                    className="p-2 flex gap-1 text-c-primary rounded hover:bg-light-secondary/60 active:bg-light-tertiary/60 transition-colors"
-                    href={`/veckans-deal`}
-                >
-                    Visa alla deals
-                    <ArrowIcon className="-mr-0.5 w-4 rotate-90" />
-                </Link>
-            </div>
-            <div className='p-4 bg-light rounded-lg'>
-                <Carousel 
-                    items={deals.map(deal => ({
-                        id: deal.id,
-                        image: getWeeklyDealImage(deal.id, deal.date),
-                        path: `/veckans-deal?deal=${deal.id}`,
-                    }))}
-                    itemsPerPage={3}
-                />
+                <div className='p-4 bg-light rounded-lg'>
+                    <Carousel 
+                        items={deals.map(deal => ({
+                            id: deal.id,
+                            image: getWeeklyDealImage(deal.id, deal.date),
+                            path: `/veckans-deal?deal=${deal.id}`,
+                        }))}
+                        itemsPerPage={3}
+                    />
+                </div>
             </div>
         </section>
     )
