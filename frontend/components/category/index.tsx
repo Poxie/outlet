c1474d37f97ff391fcd3e4257fdeff62ecc46c88
Poxie <poxen12@hotmail.com>
2023-12-10 20:57:03 +0100

    Added simple 404 page

--- frontend/components/category/index.tsx
+++ frontend/components/category/index.tsx
@@ -4,13 +4,29 @@ import EventContainer from "../event/EventContainer";
 
 const getEvents = async (categoryId: string) => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/categories/${categoryId}/children`, { next: { revalidate: 0 } });
-    return await res.json() as EventWithImages[];
+    if(res.ok) {
+        return await res.json() as EventWithImages[];
+    }
+    return null;
 }
 
 export default async function Category({ params: { categoryId } }: {
     params: { categoryId: string };
 }) {
     const events = await getEvents(categoryId);
+    if(!events) {
+        return(
+            <main className="py-24 text-center text-light bg-primary">
+                <h1 className="text-5xl font-black mb-3">
+                    404: page not found.
+                </h1>
+                <p className="text-xl">
+                    The page you were looking for has been moved or does not exist.
+                </p>
+            </main>
+        )
+    }
+
     return(
         <main className="w-main max-w-main mx-auto">
             {events.map((event, index) => (
