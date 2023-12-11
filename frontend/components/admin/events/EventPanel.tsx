cf52e41e90c4733ab373699d0c979947ddf5ed6a
Poxie <poxen12@hotmail.com>
2023-12-11 21:57:02 +0100

    Added popout to select a category

--- frontend/components/admin/events/EventPanel.tsx
+++ frontend/components/admin/events/EventPanel.tsx
@@ -2,15 +2,21 @@ import { ArrowIcon } from "@/assets/icons/ArrowIcon";
 import Button from "@/components/button";
 import Input from "@/components/input";
 import { useModal } from "@/contexts/modal";
+import { usePopout } from "@/contexts/popout";
 import { useEvents } from "@/hooks/useEvents";
 import AddEventModal from "@/modals/events/AddEventModal";
+import CategoryPopout from "@/popouts/categories";
 import { useAppSelector } from "@/store";
 import { selectCategoriesLength } from "@/store/slices/categories";
 import Link from "next/link";
+import { useRef } from "react";
 
 export default function EventPanel() {
     const { setModal } = useModal();
+    const { setPopout } = usePopout();
     const { addEvent, setSearch } = useEvents();
+
+    const categoryPopoutButton = useRef<HTMLButtonElement>(null);
 
     const categoryCount = useAppSelector(selectCategoriesLength);
 
@@ -19,14 +25,30 @@ export default function EventPanel() {
             onEventAdd={addEvent}
         />
     )
+    const openCategoryPopout = () => {
+        setPopout({
+            popout: <CategoryPopout onClick={console.log} />,
+            ref: categoryPopoutButton,
+            options: { position: 'right' },
+        })
+    }
 
     return(
         <div className="p-4 flex items-center justify-between rounded-lg bg-light shadow-centered mb-[2px]">
-            <Input 
-                className="px-2 py-2 w-[300px] max-w-full"
-                placeholder={'Search'}
-                onChange={setSearch}
-            />
+            <div className="flex gap-2">
+                <Input 
+                    className="px-2 py-2 w-[300px] max-w-full"
+                    placeholder={'Search'}
+                    onChange={setSearch}
+                />
+                <button 
+                    className="px-2 text-secondary text-sm border-[1px] bg-light-secondary hover:bg-light-tertiary/80 transition-colors border-light-tertiary rounded-md"
+                    onClick={openCategoryPopout}
+                    ref={categoryPopoutButton}
+                >
+                    Sort by category...
+                </button>
+            </div>
             <Link 
                 className="px-3 py-2 flex items-center gap-2 text-secondary hover:bg-light-secondary/50 active:bg-light-secondary transition-colors rounded-md"
                 href={`/admin/events/categories`}
---
+++ frontend/popouts/categories/index.tsx
@@ -0,0 +1,45 @@
+import { useAppSelector } from "@/store"
+import { selectCategories, selectCategoriesLength } from "@/store/slices/categories"
+import { EventCategory } from "../../../types";
+import { AddIcon } from "@/assets/icons/AddIcon";
+import Link from "next/link";
+import { usePopout } from "@/contexts/popout";
+
+export default function CategoryPopout({ onClick }: {
+    onClick: (category: EventCategory) => void;
+}) {
+    const { close } = usePopout();
+
+    const categories = useAppSelector(selectCategories);
+    const count = useAppSelector(selectCategoriesLength);
+
+    return(
+        <div>
+            <div className="p-3 border-b-[1px] border-b-light-tertiary flex items-center justify-between">
+                <span>
+                    {count} categories
+                </span>
+                <Link 
+                    onClick={close}
+                    className="p-1 hover:bg-light-secondary active:bg-light-tertiary/80 transition-colors rounded-md"
+                    href={'/admin/events/categories/create'}
+                    aria-label="Create category"
+                >
+                    <AddIcon className="w-5" />
+                </Link>
+            </div>
+            <ul className="p-2">
+                {categories.map(category => (
+                    <li key={category.id}>
+                        <button
+                            className="p-2 w-full text-left hover:bg-light-secondary/50 active:bg-light-secondary transition-colors rounded-md"
+                            onClick={() => onClick(category)}
+                        >
+                            {category.name}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
