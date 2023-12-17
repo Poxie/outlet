28ec3c3c76f2ef51e680defd4ee6328c5b856d87
Poxie <poxen12@hotmail.com>
2023-12-17 02:46:01 +0100

    Added path header to inspiration posts

--- frontend/components/inspiration/Inspiration.tsx
+++ frontend/components/inspiration/Inspiration.tsx
@@ -1,5 +1,7 @@
+import { ArrowIcon } from "@/assets/icons/ArrowIcon";
 import { BlogPost } from "../../../types";
 import InspirationPost from "./InspirationPost";
+import Link from "next/link";
 
 const getInspirationPost = async (inspirationId: string) => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/inspiration/${inspirationId}`, { next: { revalidate: 0 } });
@@ -15,7 +17,18 @@ export default async function Inspiration({
 }) {
     const post = await getInspirationPost(inspirationId);
     return(
-        <main className="py-8">
+        <main>
+            <div className="py-4 mb-4 bg-light border-b-[1px] border-b-light-secondary">
+                <div className="w-main max-w-main mx-auto flex">
+                    <Link 
+                        className="p-2 -m-2 flex items-center gap-2 hover:bg-light-secondary/60 active:bg-light-secondary transition-colors rounded"
+                        href={'/inspiration'}
+                    >
+                        <ArrowIcon className="w-4 -rotate-90" />
+                        Inspiration / {post.title}
+                    </Link>
+                </div>
+            </div>
             <div className="w-main max-w-main mx-auto">
                 <InspirationPost 
                     {...post}
