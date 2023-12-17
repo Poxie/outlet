62a8997b643dfc0d62c9bb5711caae38b9a98362
Poxie <poxen12@hotmail.com>
2023-12-17 01:48:09 +0100

    Made responsive changes to modals

--- frontend/modals/Modal.tsx
+++ frontend/modals/Modal.tsx
@@ -1,17 +1,20 @@
+import { useScreenSize } from '@/hooks/useScreenSize';
 import { motion } from 'framer-motion';
 
 export default function Modal({ children }: {
     children: React.ReactNode;
 }) {
+    const screenSize = useScreenSize();
+    const isSmall = ['xs'].includes(screenSize);
     return(
         <motion.div
-            exit={{ scale: .7, opacity: 0 }}
-            initial={{ scale: .7, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: isSmall ? 1 : .7, opacity: isSmall ? 1 : 0, translateY: isSmall ? '100dvh' : 0 }}
+            initial={{ scale: isSmall ? 1 : .7, opacity: isSmall ? 1 : 0, translateY: isSmall ? '100dvh' : 0 }}
+            animate={{ scale: 1, opacity: 1, translateY: 0 }}
             transition={{ duration: .2, ease: 'easeInOut' }}
             className="fixed z-30 top-0 left-0 w-full h-full flex justify-center items-center pointer-events-none"
         >
-            <div className="w-[600px] max-w-main max-h-full overflow-auto pointer-events-auto bg-light rounded-lg">
+            <div className="h-full sm:h-[unset] sm:w-[600px] sm:max-w-main max-h-full overflow-auto pointer-events-auto bg-light sm:rounded-lg">
                 {children}
             </div>
         </motion.div>
--- frontend/modals/image-preview/index.tsx
+++ frontend/modals/image-preview/index.tsx
@@ -2,13 +2,17 @@ import { CloseIcon } from "@/assets/icons/CloseIcon";
 import { ShareIcon } from "@/assets/icons/ShareIcon";
 import Button from "@/components/button";
 import { useModal } from "@/contexts/modal";
+import { useScreenSize } from "@/hooks/useScreenSize";
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 
 export default function ImagePreviewModal({ src, path }: {
     src: string;
     path: string;
 }) {
     const { close } = useModal();
+    const screenSize = useScreenSize();
+    const isSmall = ['xs'].includes(screenSize);
 
     const share = () => {
         navigator.share({ url: path })
@@ -17,21 +21,35 @@ export default function ImagePreviewModal({ src, path }: {
     }
 
     return(
-        <div className="relative">
-            <div className="p-3 flex items-center justify-between">
+        <div className={twMerge(
+            "relative flex flex-col-reverse sm:flex-col",
+            isSmall && 'h-full justify-between',
+        )}>
+            <div className="p-3 flex gap-3 items-center justify-between">
                 <Button
                     onClick={share}
                     icon={<ShareIcon className="w-4 -mt-0.5" strokeWidth={2} />}
-                    className="py-3 px-3 flex-row-reverse text-sm"
+                    className={twMerge(
+                        "py-3 px-3 flex-row-reverse text-sm",
+                        isSmall && 'flex-1',
+                    )}
                 >
                     Dela
                 </Button>
                 <button
                     onClick={close}
                     aria-label={'Stäng förhandsvisning'}
-                    className="aspect-square text-secondary rounded hover:bg-light-secondary hover:text-primary transition-colors"
+                    className={twMerge(
+                        "text-secondary rounded ",
+                        !isSmall && 'aspect-square',
+                        isSmall && "flex-1 p-3 hover:bg-light-secondary hover:text-primary transition-colors sm:bg-transparent bg-light-secondary hover:bg-light-tertiary/80" 
+                    )}
                 >
-                    <CloseIcon className="w-8" />
+                    {!isSmall ? (
+                        <CloseIcon className="w-8" />
+                    ) : (
+                        'Stäng'
+                    )}
                 </button>
             </div>
             <Image 
