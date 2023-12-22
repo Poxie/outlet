8866123fa2a9f952a83574830aad99567823f652
Poxie <poxen12@hotmail.com>
2023-12-23 00:50:02 +0100

    Added opengraph attributes to meta data

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,13 +1,21 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
 import Navbar from '@/components/navbar'
 import Banner from '@/components/banner';
 import Providers from '@/contexts';
 
+const title = 'Åhlens Outlet';
+const description = 'Vi erbjuder mode, hem och skönhet till 30-70 procents lägre pris än i övriga butiker. I våra varuhus finns såväl etablerade varumärken som vi är vana att se på Åhléns, liksom nya och tillfälliga kollektioner.';
 export const metadata: Metadata = {
-  title: 'Åhlens Outlet',
-  description: 'Vi erbjuder mode, hem och skönhet till 30-70 procents lägre pris än i övriga butiker. I våra varuhus finns såväl etablerade varumärken som vi är vana att se på Åhléns, liksom nya och tillfälliga kollektioner.',
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    siteName: title,
+    locale: 'sv-SE',
+    url: process.env.NEXT_PUBLIC_SITE_URL,
+  }
 }
 
 export default function RootLayout({
