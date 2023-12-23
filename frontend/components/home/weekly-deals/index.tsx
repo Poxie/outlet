6fe7f52a2ee2458e441a6f1802efd893b75a72b4
Poxie <poxen12@hotmail.com>
2023-12-23 01:31:57 +0100

    Hiding overflow caused by carousel buttons

--- frontend/components/home/weekly-deals/index.tsx
+++ frontend/components/home/weekly-deals/index.tsx
@@ -23,33 +23,35 @@ export default async function WeeklyDeals() {
     const endString = `${date.getDate()} ${month}`;
 
     return(
-        <section className="py-8 w-main max-w-main mx-auto">
-            <div className="mb-2 py-4 sm:py-2 px-4 grid sm:flex sm:gap-3 items-center text-sm bg-light rounded-lg">
-                <span className="text-2xl sm:text-xl font-semibold">
-                    Veckans deal
-                </span>
-                <span className="text-secondary mt-1">
-                    Gäller t.o.m. {endString}. Risk för utsäljning.
-                </span>
-                <div className="flex-1 flex sm:justify-end mt-1 sm:mt-0">
-                    <Link 
-                        className="p-2 -ml-2 sm:ml-0 -mb-2 sm:mb-0 sm:-mr-2 flex items-center gap-1 text-secondary rounded hover:bg-light-secondary/80 active:bg-light-tertiary/80 transition-colors"
-                        href={`/veckans-deal`}
-                    >
-                        Visa alla deals
-                        <ArrowIcon className="-mr-0.5 w-4 rotate-90" />
-                    </Link>
+        <section className="overflow-hidden">
+            <div className="py-8 w-main max-w-main mx-auto">
+                <div className="mb-2 py-4 sm:py-2 px-4 grid sm:flex sm:gap-3 items-center text-sm bg-light rounded-lg">
+                    <span className="text-2xl sm:text-xl font-semibold">
+                        Veckans deal
+                    </span>
+                    <span className="text-secondary mt-1">
+                        Gäller t.o.m. {endString}. Risk för utsäljning.
+                    </span>
+                    <div className="flex-1 flex sm:justify-end mt-1 sm:mt-0">
+                        <Link 
+                            className="p-2 -ml-2 sm:ml-0 -mb-2 sm:mb-0 sm:-mr-2 flex items-center gap-1 text-secondary rounded hover:bg-light-secondary/80 active:bg-light-tertiary/80 transition-colors"
+                            href={`/veckans-deal`}
+                        >
+                            Visa alla deals
+                            <ArrowIcon className="-mr-0.5 w-4 rotate-90" />
+                        </Link>
+                    </div>
                 </div>
-            </div>
-            <div className='p-4 bg-light rounded-lg mb-[calc(3rem+.5rem)] md:mb-0'>
-                <Carousel 
-                    items={deals.map(deal => ({
-                        id: deal.id,
-                        image: getWeeklyDealImage(deal.id, deal.parentId),
-                        path: `/veckans-deal?deal=${deal.id}`,
-                    }))}
-                    itemsPerPage={3}
-                />
+                <div className='p-4 bg-light rounded-lg mb-[calc(3rem+.5rem)] md:mb-0'>
+                    <Carousel 
+                        items={deals.map(deal => ({
+                            id: deal.id,
+                            image: getWeeklyDealImage(deal.id, deal.parentId),
+                            path: `/veckans-deal?deal=${deal.id}`,
+                        }))}
+                        itemsPerPage={3}
+                    />
+                </div>
             </div>
         </section>
     )
